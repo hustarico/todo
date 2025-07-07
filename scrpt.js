dd97c363208dc9a131f6b5a759814aac24c3e1e9
hussam tarteer <hussamtarteer74@gmail.com>
2025-07-08 02:12:14 +0300

    first push

---
+++ scrpt.js
@@ -0,0 +1,73 @@
+let todoArr = JSON.parse(localStorage.getItem("todos")) || [];
+todoArr = todoArr.filter((todo)=>todo.done==false);
+localStorage.setItem("todos",JSON.stringify(todoArr))
+
+
+let todoList = document.querySelector(".todo-list");
+
+todoArr.forEach(todo => {
+    if(todo.done == false){
+
+        //? adding child
+        let newToDo = document.createElement('li');
+        newToDo.innerHTML='<div class="todo"><p>'+todo.text+'</p><input type="checkbox"></div>';
+
+        //?giving child an event listener
+        let newCheck = newToDo.firstElementChild.lastElementChild;
+        newCheck.addEventListener("change",dashOut);
+
+        todoList.appendChild(newToDo);
+    }
+});
+
+function dashOut(){
+    /** @type {HTMLElement} **/
+    let siblingText =this.previousElementSibling ;
+    console.log(siblingText);
+    siblingText.classList.toggle("dashedOut");
+
+
+
+    todoArr.forEach(todo =>{
+        console.log(todo);
+        if(todo.text.trim() == siblingText.innerText.trim())
+            todo.done=!todo.done;
+
+    })
+    localStorage.setItem("todos",JSON.stringify(todoArr));
+}
+
+//////////////////////////////////////////////////!
+
+
+document.querySelector("form").addEventListener("submit",addTask);
+
+function addTask(event){
+    event.preventDefault();
+    
+    /** @type {HTMLFormElement} **/
+    let inputField = this.firstElementChild;
+
+    if(inputField.value == "")return;
+
+    let newTodo = {
+        text:inputField.value,
+        done:false
+    };
+
+    //? adding child
+    let newTodoElement = document.createElement('li');
+    newTodoElement.innerHTML='<div class="todo"><p>'+newTodo.text+'</p><input type="checkbox"></div>';
+
+    //?giving child an event listener
+    let newCheck = newTodoElement.firstElementChild.lastElementChild;
+    newCheck.addEventListener("change",dashOut);
+
+    todoList.appendChild(newTodoElement);
+    
+
+    todoArr.push(newTodo);
+    localStorage.setItem("todos",JSON.stringify(todoArr));
+    
+    inputField.value = "";
+}
